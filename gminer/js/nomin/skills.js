3634f2d5ea46ab268c245403d3f7baad03552045
Legion9378 <66057599+Legion9378@users.noreply.github.com>
2021-06-14 19:40:59 +0200

    Update skills.js

--- gminer/js/nomin/skills.js
+++ gminer/js/nomin/skills.js
@@ -5,28 +5,28 @@ skills = {
     "id": 0,
     "name": "The Golden Axe",
     "description": "Increases your gold/s rate.",
-    "value": 1,
+    "value": 3,
     "valueSuffix": "%"
   },
   'jeweler': {
     "id": 1,
     "name": "The Jeweler",
     "description": "Increases your diamond/s rate.",
-    "value": 1,
+    "value": 3,
     "valueSuffix": "%"
   },
   'blacksmith': {
     "id": 2,
     "name": "The Blacksmith",
     "description": "Increases the stats of the items you find in the future.",
-    "value": 1,
+    "value": 3,
     "valueSuffix": "%"
   },
   'trainer': {
     "id": 3,
     "name": "The Trainer",
     "description": "Decreases the minion prices.",
-    "value": 0.2,
+    "value": 1.2,
     "valueSuffix": "%"
   }
 };
